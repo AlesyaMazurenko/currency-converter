aea9a18c90d2c65e517a6a9007e1476506eefda4
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-12-26 12:23:17 +0100

    search function

--- script.js
+++ script.js
@@ -69,18 +69,24 @@ function onSubmit() {
 
 function searchFunction() {
   // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
+  let input, filter, table, tr, td, i, txtValue, txtValue1;
+  input = document.getElementById("searchInp");
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById("rateTable");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    td1 = tr[i].getElementsByTagName("td")[1];
+
+    if (td || td1) {
+     txtValue = td.textContent || td.innerText;
+     txtValue1 = td1.textContent || td1.innerText;
+
+      if (txtValue.indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else if (txtValue1.indexOf(filter) > -1) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
